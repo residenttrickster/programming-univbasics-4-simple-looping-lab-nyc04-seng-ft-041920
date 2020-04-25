a173717b0c8b291ce6b81a557543398a58b9010d
Anastasia Orlova <metkusblackus@gmail.com>
2020-04-25 19:50:54 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,7 +1,16 @@
 def loop_message_five_times(string)
   count = 0
-  while count <=5
+  while count <= 5
   puts string
   count +=1
   end
 end
+
+def loop_message_n_times(string, n)
+  count = 0
+  while count <= n
+    puts string
+  end
+end
+
+  
