939cae0ae15dd4fe03070716c1902c5874ac72df
Anastasia Orlova <metkusblackus@gmail.com>
2020-04-25 20:02:22 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,6 +1,6 @@
 def loop_message_five_times(string)
   count = 0
-  while count <= 5
+  while count <= 5 do
   puts string
   count +=1
   end
@@ -8,11 +8,20 @@ end
 
 def loop_message_n_times(string, n)
   count = 0
-  while count <= n
+  while count <= n do 
     puts string
     count +=1
   end
 end
 
 def output_array(array)
+  count = 0 
+  while array [count] do 
+    puts array [count]
+    count +=1
+  end 
+end
+
+
+def return_string_array
 end
